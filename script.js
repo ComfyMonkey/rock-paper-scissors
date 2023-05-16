9d5ad929e1a83cc7e08b5a7e0a3dd70f46e70099
Daniel <daniel.michailenko@gmail.com>
2023-05-16 12:03:59 +0200

    Adds function to play a round of RPS with the computer selection

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 
 
-
-
-
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -16,5 +13,26 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "DRAW!";
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        return "Player Wins!";
+    } else {
+        return "Computer Wins!"
+    }
+}
+
+
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice();
+
+  console.log(playRound(playerSelection, computerSelection));
+
+
 
