c2ee46b849d0a8251b9a23f0f859bea46be3418a
Daniel <daniel.michailenko@gmail.com>
2023-05-17 16:07:21 +0200

    Adds game function and plays five rounds of RPS

--- script.js
+++ script.js
@@ -28,10 +28,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
-
-  console.log(playRound(playerSelection, computerSelection));
-
+function game() {
+    let computerScore = 0;
+    let playerScore = 0; 
 
 
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose between Rock, Paper, Scissors");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection)); 
+
+        if (playRound(playerSelection, computerSelection) == `Player Wins! ${playerSelection} beats ${computerSelection}.`) {
+            playerScore++
+        }
+        else if (playRound(playerSelection, computerSelection) == `Computer Wins! ${playerSelection} loses against ${computerSelection}.`) {
+            computerScore++;
+        }
+    }
+    console.log("Game Over")
+    if(playerScore > computerScore) {
+        console.log("Player was the winner!");
+    }
+    else if(playerScore < computerScore) {
+        console.log("Computer was the winner!");
+    }
+    else {
+        console.log("There is a tie!")
+    }
+}
+
+game();
